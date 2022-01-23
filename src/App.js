ee4077f7380d78e1885810032c8e6742b258e43d
Danil <75380923+OGLwqx@users.noreply.github.com>
2022-01-23 19:47:57 +0500

    Добавлен блок, отображающий победивший предмет

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 
 import Blocks from './Components/Blocks';
 
+import box_01 from './Components/box_1.png';
+import box_02 from './Components/box_2.png';
+import box_03 from './Components/box_3.png';
+import box_04 from './Components/box_4.png';
+import box_05 from './Components/box_5.png';
 
 const App = () => {
 
@@ -10,10 +15,51 @@ const App = () => {
   const [getPrize, setGetPrize] = useState(false);
   const [prizeNum, setPrizeNum] = useState('')
   const [lastWinNum, setLastWinNum] = useState(1)
+  const [items, setItemsList] = useState(null);
 
-  function setPrize(data) {
-    setGetPrize(data);
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+useEffect(() => {
+  setTimeout(() => {
+    var elements = []
+    for (let index = 1; index < 500; index++) {
+        var element = document.createElement("div");
+        //element.innerHTML = `<span>${index}</span>`;
+        element.setAttribute("class", "block__default");
+        element.setAttribute("id", `${index}`);
+        document.getElementById("list").appendChild(element);
+        var element2 = document.createElement("img");
+        var image = getRandomInRange(1,5);
+        switch (image) {
+            case 1:
+                element2.setAttribute("src", box_01);
+                elements.push({id: index, item: box_01})
+                break;
+            case 2:
+                element2.setAttribute("src", box_02);
+                elements.push({id: index, item: box_02})
+                break;
+            case 3:
+                element2.setAttribute("src", box_03);
+                elements.push({id: index, item: box_03})
+                break;
+            case 4:
+                element2.setAttribute("src", box_04);
+                elements.push({id: index, item: box_04})
+                break;
+            case 5:
+                element2.setAttribute("src", box_05);
+                elements.push({id: index, item: box_05})
+                break;
+        }
+        document.getElementById(index).appendChild(element2);
+    }
+    setItemsList(elements)
+  }, 0);
+}, [])
+
 
   var pixels = 0
 
@@ -28,23 +74,21 @@ const App = () => {
     var start_time = parseInt((Date.now() / 1000).toFixed(0))
     setGetPrize(false)
     var speedStep = speed / 10
-    var timeStep = (duration * 1000) / 40
+    var timeStep = (duration * 1000) / 20
     setTimeout(() => {
       var slowing = setInterval(() => {
-        if(speed <= 0) {
+        if(speed - speedStep <= 0) {
           speed = 0;
           clearInterval(slowing)
           return;
         }
         speed -= speedStep
-        console.log(speed)
+        //console.log(speed)
       }, timeStep);
-    }, (duration * 1000) * 0.75);
+    }, (duration * 1000) * 0.50);
     var timer = setInterval(() => {
-      //console.log(start_time + duration - (Date.now() / 1000))
       if(parseInt((Date.now() / 1000).toFixed(0)) - start_time > duration) {
         clearInterval(timer)
-        //console.log(true)
         setGetPrize(true)
         var blocks = document.getElementsByClassName('block__default')
         var blocks_array = Array.prototype.slice.call( blocks )
@@ -52,14 +96,15 @@ const App = () => {
         for (let index = 0; index < blocks_array.length; index++) {
           const c = blocks_array[index];
           const n = blocks_array[index+1]
-          //console.log(c.offsetLeft)
           if(pixels > c.offsetLeft && pixels < n.offsetLeft)  {
-            console.log('win')
             setTimeout(() => {
-              console.log('win_animation', index+1)
+              var showPrize = document.createElement("img");
+              showPrize.setAttribute("id", 'winItem');
+              var winItem = items[index]
+              showPrize.setAttribute("src", winItem.item);
+              document.getElementById("cell").appendChild(showPrize);
+              console.log(winItem)
               console.log(pixels)
-              //console.log(index+1)
-              //console.log(pixels, c.offsetLeft, n.offsetLeft)
               setPrizeNum(index+1)
               var prizeBlock = document.getElementById(index+1)
               setLastWinNum(index+1)
@@ -68,6 +113,7 @@ const App = () => {
               changeSpeed2 = true
               setTimeout(() => {
                 prizeBlock.setAttribute("style", `transform: scale(1)`);
+                document.getElementById("winItem").remove();
                 setGetPrize(false)
               }, 5000);
             }, 700);
--- src/Components/Blocks.js
+++ src/Components/Blocks.js
@@ -2,47 +2,9 @@ import React from 'react';
 
 import '../App.css';
 
-import box_01 from './box_1.png';
-import box_02 from './box_2.png';
-import box_03 from './box_3.png';
-import box_04 from './box_4.png';
-import box_05 from './box_5.png';
-
 function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-
-setTimeout(() => {
-    for (let index = 1; index < 2500; index++) {
-        var element = document.createElement("div");
-        //element.innerHTML = `<span>${index}</span>`;
-        element.setAttribute("class", "block__default");
-        element.setAttribute("id", `${index}`);
-        document.getElementById("list").appendChild(element);
-        var element2 = document.createElement("img");
-        var image = getRandomInRange(1,5);
-        switch (image) {
-            case 1:
-                element2.setAttribute("src", box_01);
-                break;
-            case 2:
-                element2.setAttribute("src", box_02);
-                break;
-            case 3:
-                element2.setAttribute("src", box_03);
-                break;
-            case 4:
-                element2.setAttribute("src", box_04);
-                break;
-            case 5:
-                element2.setAttribute("src", box_05);
-                break;
-        }
-        document.getElementById(index).appendChild(element2);
-    }
-  }, 0);
-
-
 
 const Blocks = props => (
 	<div className='background'>
@@ -51,6 +13,11 @@ const Blocks = props => (
         : 
         <div></div>
         }
+        <div className='item-cell__wrapper'>
+            <div className='item-cell' id='cell'>
+                
+            </div>
+        </div>
         <div className='arrow__wrapper'>
             <div class="arrow" id='arrow'>
                 <span class="arrow-left"></span>
